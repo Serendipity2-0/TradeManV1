0d674a28fda3ada5f5f65f50b5843b288248b5aa
traderscafe11 <amol37007@gmail.com>
2024-02-22 10:28:14 +0530

    Update zerodha_adapter.py

--- Executor/ExecutorUtils/BrokerCenter/Brokers/Zerodha/zerodha_adapter.py
+++ Executor/ExecutorUtils/BrokerCenter/Brokers/Zerodha/zerodha_adapter.py
@@ -44,21 +44,24 @@ def zerodha_fetch_free_cash(user_details):
     logger.debug(f"Fetching free cash for {user_details['BrokerUsername']}")
     kite = KiteConnect(api_key=user_details["ApiKey"])
     kite.set_access_token(user_details["SessionId"])
-    # Fetch the account balance details
     try:
+        # Fetch the account balance details
         balance_details = kite.margins(segment="equity")
-        cash_balance = balance_details["cash"]
+        # Extract the 'cash' value
+        cash_balance = balance_details.get("cash", 0)
+
+        # If 'cash' key is not at the top level, we need to find where it is
         if cash_balance == 0 and "cash" not in balance_details:
             # Look for 'cash' in nested dictionaries
             for key, value in balance_details.items():
                 if isinstance(value, dict) and "cash" in value:
                     cash_balance = value.get("cash", 0)
                     break
+        logger.debug(f"Free cash fetched for {user_details['BrokerUsername']}: {cash_balance}")
+        return cash_balance
     except Exception as e:
         logger.error(f"Error fetching free cash: {e} for {user_details['BrokerUsername']}")
         return 0
-    logger.info(f"Free cash for {user_details['BrokerUsername']}: {cash_balance}")
-    return cash_balance
 
 
 def get_csv_kite(user_details):
