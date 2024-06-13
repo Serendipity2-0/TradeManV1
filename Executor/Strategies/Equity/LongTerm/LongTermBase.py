e36bdfb7ccbc381623b570a8aa4c38e3b5f77c7e
1Chinmay1 <phchinmay80@gmail.com>
2024-06-13 15:26:55 +0530

    Restructered Equity and added logger functions

--- Executor/Strategies/Equity/LongTerm/LongTermBase.py
+++ Executor/Strategies/Equity/LongTerm/LongTermBase.py
@@ -11,19 +11,25 @@ ENV_PATH = os.path.join(DIR, "trademan.env")
 load_dotenv(ENV_PATH)
 
 from Executor.ExecutorUtils.LoggingCenter.logger_utils import LoggerSetup
-from Executor.Strategies.Equity.EquityBase import get_financial_data,get_stock_codes
+from Executor.ExecutorUtils.EquityCenter.EquityCenterUtils import (
+    get_financial_data,
+    get_stock_codes,
+)
+
 logger = LoggerSetup()
 
 stock_codes = get_stock_codes()
-stock_codes=stock_codes[:20]
-stock_financial_data_df =get_financial_data(stock_codes)
+stock_codes = stock_codes[:20]
+stock_financial_data_df = get_financial_data(stock_codes)
 if not stock_financial_data_df.empty:
     # SQLite database path
     financial_db_path = os.getenv("financial_db_path")
-    table_name = 'financials'
+    table_name = "financials"
     try:
         conn = sqlite3.connect(financial_db_path)
-        stock_financial_data_df.to_sql(table_name, conn, if_exists='replace', index=False)
+        stock_financial_data_df.to_sql(
+            table_name, conn, if_exists="replace", index=False
+        )
         logger.info(f"Data uploaded to {table_name} table in {financial_db_path}")
     except Exception as e:
         logger.error(f"Error uploading data to SQLite: {e}")
--- Executor/Strategies/Equity/ShortTerm/ShortTerm.py
+++ Executor/Strategies/Equity/ShortTerm/ShortTerm.py
@@ -10,10 +10,17 @@ ENV_PATH = os.path.join(DIR, "trademan.env")
 load_dotenv(ENV_PATH)
 
 from Executor.ExecutorUtils.LoggingCenter.logger_utils import LoggerSetup
-from Executor.Strategies.Equity.ShortTerm.EmaBBConfluenceStrategy import perform_EmaBB_Confluence_strategy
-from Executor.Strategies.Equity.ShortTerm.MomentumStrategy import perform_momentum_strategy
-from Executor.Strategies.Equity.ShortTerm.MeanReversionStrategy import perform_mean_reversion_strategy
-from Executor.Strategies.Equity.EquityBase import read_stock_data_from_db,update_todaystocks_db,merge_dataframes
+from Executor.Strategies.Equity.ShortTerm.ShortTermUtils import (
+    perform_EmaBB_Confluence_strategy,
+    perform_mean_reversion_strategy,
+    perform_momentum_strategy,
+)
+from Executor.ExecutorUtils.EquityCenter.EquityCenterUtils import (
+    read_stock_data_from_db,
+    update_todaystocks_db,
+    merge_dataframes,
+)
+
 logger = LoggerSetup()
 
 
@@ -29,17 +36,17 @@ def main():
     Returns:
         list: A sorted list of short term stock picks.
     """
-    global momentum_stocks_df
-    global mean_reversion_stocks_df
-    global ema_bb_confluence_stocks_df
     # Example usage
-    db_path = 'stock_data.db'
+    db_path = os.getenv("equity_stock_data_db_path")
     stock_data_dict = read_stock_data_from_db(db_path)
     momentum_stocks_df = perform_momentum_strategy(stock_data_dict)
     mean_reversion_stocks_df = perform_mean_reversion_strategy(stock_data_dict)
     ema_bb_confluence_stocks_df = perform_EmaBB_Confluence_strategy(stock_data_dict)
-    merged_df = merge_dataframes(momentum_stocks_df,mean_reversion_stocks_df,ema_bb_confluence_stocks_df)
+    merged_df = merge_dataframes(
+        momentum_stocks_df, mean_reversion_stocks_df, ema_bb_confluence_stocks_df
+    )
     update_todaystocks_db(combined_df=merged_df)
-    print(merged_df.columns)
+
+
 if "__main__" == __name__:
     main()
