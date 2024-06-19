77ffb6f15eb74aaf1259185099f2c12aedb36f85
amolkittur <47496838+amolkittur@users.noreply.github.com>
2024-06-19 10:29:15 +0530

    Update app.py

--- User/UserApi/app.py
+++ User/UserApi/app.py
@@ -51,7 +51,7 @@ def register_user(user_detail: schemas.UserDetails):
         user_detail (schemas.UserDetails): An object containing the user's details.
     """
     user_detail = dict(user_detail)
-    print(user_detail)
+    logger.debug(user_detail)
     # update_new_client_data_to_db(get_next_trader_number(), user_detail)
     return {"message": "User registered successfully"}
 
