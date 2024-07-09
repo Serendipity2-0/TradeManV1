c77886e5fe19a1ae2272ccae866aa42dfb8a3ade
amolkittur <47496838+amolkittur@users.noreply.github.com>
2024-07-09 13:11:29 +0530

    Test for github projects (#35)

--- User/UserApi/main.py
+++ User/UserApi/main.py
@@ -585,7 +585,7 @@ app_fastapi.include_router(app_admin, prefix="/v1/admin", tags=["admin"])
 
 
 def main_api():
-    uvicorn.run("main:app_fastapi", host="0.0.0.0", port=8002, reload=True)
+    uvicorn.run("main:app_fastapi", host="0.0.0.0", port=8082, reload=False)
 
 
 if __name__ == "__main__":
