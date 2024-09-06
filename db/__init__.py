a769a5aeececcba21e2642287234033ef7cfe8b6
SatayrthReconect <satyarth.raghuvanshi@reconect.ai>
2024-09-06 11:02:40 +0530

    ♻️

--- db/__init__.py
+++ db/__init__.py
@@ -15,18 +15,19 @@ ENV_PATH = os.path.join(DIR, "..", "trademan.env")
 load_dotenv(ENV_PATH)
 
 db_config = {
-    'postgres': {
-        'user': os.getenv('DB_USER'),
-        'password': os.getenv('DB_PASSWORD'),
-        'host': os.getenv('DB_HOST'),
-        'port': os.getenv('DB_PORT', '5432'),
-        'name': os.getenv('DB_NAME'),
+    "postgres": {
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT", "5432"),
+        "name": os.getenv("DB_NAME"),
     },
-    'mongo': {
-        'user': os.getenv('MONGO_USER'),
-        'password': os.getenv('MONGO_PASSWORD'),
-        'host': os.getenv('MONGO_HOST'),
-        'port': os.getenv('MONGO_PORT', '27017'),
-        'name': os.getenv('MONGO_DB'),
-    }
+    "mongo": {},
+    # 'mongo': {
+    #     'user': os.getenv('MONGO_USER'),
+    #     'password': os.getenv('MONGO_PASSWORD'),
+    #     'host': os.getenv('MONGO_HOST'),
+    #     'port': os.getenv('MONGO_PORT', '27017'),
+    #     'name': os.getenv('MONGO_DB'),
+    # }
 }
